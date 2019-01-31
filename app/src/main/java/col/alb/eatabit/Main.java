95816d2efda7ac9ecb4d596adc555787d1055b6a
albycol <colopi.alberto@gmail.com>
2019-01-31 15:27:25 +0100

    Initial commit

---
+++ app/src/main/java/col/alb/eatabit/Main.java
@@ -0,0 +1,127 @@
+package col.alb.eatabit;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginBtn;
+    Button registerBtn;
+
+    EditText emailEt;
+    EditText passwordEt;
+
+    Switch switchDark;
+
+    LinearLayout linearL;
+
+    TextView textdarkm;
+    final static int len_pass = 6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //initialize UI controller
+        emailEt = findViewById(R.id.email_et);
+        passwordEt = findViewById(R.id.password_et);
+        loginBtn = findViewById(R.id.loginBtn);
+        registerBtn = findViewById(R.id.register);
+        switchDark=findViewById(R.id.switchb);
+        linearL=findViewById(R.id.linearl);
+        textdarkm=findViewById(R.id.text_dark);
+
+
+
+
+
+        if (hasIvitationCode()) {
+            registerBtn.setVisibility(View.VISIBLE);
+        }
+
+        loginBtn.setOnClickListener(this);
+        registerBtn.setOnClickListener(this);
+        textdarkm.setOnClickListener(this);
+        switchDark.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    linearL.setBackgroundResource(R.color.dark);
+                    textdarkm.setTextColor(getResources().getColor(R.color.light));
+                    emailEt.setTextColor(getResources().getColor(R.color.light));
+                    passwordEt.setTextColor(getResources().getColor(R.color.light));
+                }
+                else{
+                    linearL.setBackgroundResource(R.color.light);
+                    textdarkm.setTextColor(getResources().getColor(R.color.dark));
+                    emailEt.setTextColor(getResources().getColor(R.color.dark));
+                    passwordEt.setTextColor(getResources().getColor(R.color.dark));
+                }
+            }
+        });
+
+
+        Log.i("Main Activity", "Activity created");
+    }
+
+    private boolean hasIvitationCode() {
+        return true;
+    }
+
+    private boolean checkEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        //TODO do something
+        if (view.getId() == R.id.loginBtn) {
+            if(doLogin()){
+                /*Intent intent = new Intent(this, col.alb.eatabit.Welcome.class);
+                intent.putExtra("email",emailEt.getText().toString());
+                startActivity(intent);*/
+            }
+        }else if(view.getId()==R.id.register){
+            Intent intent = new Intent(this,Register.class);
+            startActivity(intent);
+        }
+    }
+    private boolean doLogin() {
+        if (!(checkEmail(emailEt.getText().toString()))) {
+            showText("Email non valida");
+            return false;
+        }
+
+        if (!(passwordEt.getText().toString().length() > len_pass)) {
+            showText("Password errata");
+            return false;
+        }
+
+        showText("Email valida");
+        return true;
+
+
+
+    }
+    private void showText(String str) {
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+    }
+
+}
+
---
+++ app/src/main/java/col/alb/eatabit/Register.java
@@ -0,0 +1,77 @@
+package col.alb.eatabit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.regex.Pattern;
+
+public class Register extends AppCompatActivity implements TextWatcher {
+
+    EditText emailEt;
+    EditText passwordEt;
+    EditText phoneEt;
+    Button signinBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        emailEt = findViewById(R.id.mail1);
+        emailEt.addTextChangedListener(this);
+        passwordEt = findViewById(R.id.password);
+        passwordEt.addTextChangedListener(this);
+        phoneEt = findViewById(R.id.phonenumb);
+        phoneEt.addTextChangedListener(this);
+        signinBtn = findViewById(R.id.signin);
+
+
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private boolean checkEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean checkPhone(String ph){
+        return Patterns.PHONE.matcher(ph).matches();
+    }
+    private void validate() {
+        String e = emailEt.getText().toString();
+        String p = passwordEt.getText().toString();
+        String ph = phoneEt.getText().toString();
+        if (checkEmail(e)&& p.length()>6 && checkPhone(ph)) {
+            signinBtn.setEnabled(true);
+
+        }
+        else{
+            signinBtn.setEnabled(false);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        validate();
+    }
+}
